7843cd07fee46b414daeb33fd62d672183a64cc4
Dimitar Mitev <d.mitev920123@gmail.com>
2023-05-11 10:42:04 +0300

    02. Insert Data in Tables

---
+++ 01. Data Definition And Data Types - Lab/02. insert-data-in-tables.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees (first_name, last_name) VALUES ('test1','test1');
+INSERT INTO employees (first_name, last_name) VALUES ('test2','test2');
+INSERT INTO employees (first_name, last_name) VALUES ('test3','test3');
