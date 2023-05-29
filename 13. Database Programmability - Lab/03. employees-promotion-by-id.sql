8635830e0078f3fea24f4a057846dbcb068d8916
Dimitar Mitev <d.mitev920123@gmail.com>
2023-05-29 10:33:53 +0300

    3. Employees Promotion By ID

---
+++ 13. Database Programmability - Lab/03. employees-promotion-by-id.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE `usp_raise_salary_by_id` (id INT)
+BEGIN
+	START TRANSACTION;
+    IF((SELECT count(employee_id) FROM employees WHERE employee_id LIKE id)<> 1) THEN ROLLBACK;
+    ELSE
+		UPDATE employees AS e SET salary = salary + salary * 0.05
+        WHERE e.employee_id = id;
+	END IF;
+END
