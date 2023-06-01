fc2ce464b25d76b472b9006a6cc8998c883fce93
Dimitar Mitev <d.mitev920123@gmail.com>
2023-06-01 17:12:39 +0300

    02. Insert

---
+++ 15. Exams/05. Database Basics MySQL Exam - 20 June 2021/02. insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO clients (full_name, phone_number)
+SELECT 
+	CONCAT(drivers.first_name, ' ', drivers.last_name) AS full_name ,  
+    CONCAT('(088) 9999', drivers.id * 2) AS phone_number
+FROM drivers
+WHERE drivers.id BETWEEN 10 AND 20;
