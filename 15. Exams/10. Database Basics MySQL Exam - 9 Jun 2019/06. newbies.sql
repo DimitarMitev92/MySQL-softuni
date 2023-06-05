13637ea847c023d4d75742e6b3859b533ae354f9
Dimitar Mitev <d.mitev920123@gmail.com>
2023-06-05 14:02:23 +0300

    06. Newbies

---
+++ 15. Exams/10. Database Basics MySQL Exam - 9 Jun 2019/06. newbies.sql
@@ -0,0 +1,11 @@
+SELECT 
+    id,
+    CONCAT(first_name, ' ', last_name) AS full_name, 
+    CONCAT('$',salary) AS salary,
+    started_on
+FROM
+    employees
+WHERE
+    employees.salary >= 100000
+        AND started_on >= '2018-01-01'
+ORDER BY salary DESC , id ASC;
